b04c1eed3fe5c2c0a1b44d3f1e776073cd9dc289
aqua <aquarell.dev@gmail.com>
2024-04-15 21:45:30 +0300

    error screen

--- src/components/screens/error/index.tsx
+++ src/components/screens/error/index.tsx
@@ -1,13 +1,13 @@
 import { useRouteError } from 'react-router-dom'
 
 export default function Error() {
-	const error = useRouteError() as { status: number }
+	const error = useRouteError() as { status: number; statusText: string }
 
 	console.log(error)
 
 	return (
 		<div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-			<p className='text-7xl font-medium'>{error.status}</p>
+			<p className='text-7xl font-medium'>{error.status || error.statusText}</p>
 			<p className='text-xl italic'>Упс! Ошибка...</p>
 		</div>
 	)
