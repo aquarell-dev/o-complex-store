2ae9fbdc722391a523edf8b7391b547c46f3cfa9
aqua <aquarell.dev@gmail.com>
2024-04-15 21:43:18 +0300

    infinite scroll load products hook

---
+++ src/components/screens/home/hooks/useProducts.ts
@@ -0,0 +1,41 @@
+import { axiosClassic } from '@api/axios'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+import type { ProductResponse } from 'src/types/product.types'
+
+const useProducts = () => {
+	const [maxPage, setMaxPage] = useState(0)
+
+	const getProducts = async (page: number) => {
+		await new Promise(resolve => setTimeout(resolve, 1000))
+
+		const response = await axiosClassic.get<ProductResponse>(
+			`/products?page=${page}&page_size=12`,
+		)
+
+		if (response.status !== 200)
+			throw new Error('Не удалось получить список товаров')
+
+		setMaxPage(Math.ceil(response.data.total / response.data.amount))
+
+		return response.data
+	}
+
+	const {
+		data: products,
+		fetchNextPage,
+		hasNextPage,
+		isLoading,
+		isPending,
+	} = useInfiniteQuery({
+		queryKey: ['products'],
+		queryFn: async ({ pageParam }) => await getProducts(pageParam),
+		initialPageParam: 0,
+		getNextPageParam: lastPage =>
+			lastPage.page === maxPage ? null : lastPage.page + 1,
+	})
+
+	return { products, fetchNextPage, hasNextPage, isLoading, isPending }
+}
+
+export default useProducts
